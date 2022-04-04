3ae03a732957b7be1eb50e8047483eb6dcba31df
Jim Eli <31859133+JimEli@users.noreply.github.com>
2022-04-03 18:26:02 -0600

    Update avltree.h

--- avltree.h
+++ avltree.h
@@ -1,222 +1,296 @@
-#ifndef _SET_H_
-#define _SET_H_
+#ifndef _AVLTREE_H_
+#define _AVLTREE_H_
 
-#include <algorithm>
-#include <iostream>
-#include "avl_tree.h"
 
-template <class T>
-class set : public AVLTree<T>
+template <class T> 
+struct less { bool operator() (const T& x, const T& y) const { return x < y; } };
+
+
+template <typename T>
+int comp(T a, T b)
 {
-    AVLTree<T> body;
-    AVLnode<T>* root = nullptr;
-    size_t sz = 0;
+    if (a < b)
+        return -1;
 
-public:
-    class iterator
+    if (b < a)
+        return 1;
+    
+    return 0;
+}
+
+
+template <typename T>
+struct AVLnode
+{
+    T key;
+    int balance = 0, height = 1;
+    AVLnode* left = nullptr;
+    AVLnode* right = nullptr;
+    AVLnode* parent = nullptr;
+
+    AVLnode(T k, AVLnode* p = nullptr) : key(k), parent(p) { }
+    AVLnode(T k, int h, int b) : key(k), height(h), balance(b) { }
+
+    ~AVLnode()
     {
-        AVLnode<T>* node = nullptr;
-        const set* owner = nullptr;
+        delete left;
+        delete right;
+    }
 
-    public:
-        iterator() = default;
-        iterator(AVLnode<T>* in, const set* own) : node(in), owner(own) { }
-        iterator(const iterator& other) : node(other.node), owner(other.owner) { }
-
-        T& operator* () const { return node->key; }
-        T* operator-> () const { return &(node->key); }
-        T* operator& () const { return (&node)->key;  }
-
-        iterator& operator++ () 
+    AVLnode* nextNode()
+    {
+        AVLnode* ans = this;
+        if (right != nullptr)
         {
-            if (node == nullptr) 
-                return *this;
-            node = node->nextNode();
-            return *this;
+            ans = right;
+            while (ans->left != nullptr)
+                ans = ans->left;
         }
-        iterator operator++ (int)
+        else
         {
-            iterator cur = *this;
-            ++* this;
-            return cur;
-        }
+            AVLnode* cur = ans->parent;
 
-        iterator& operator-- () 
-        {
-            if (node == nullptr) 
+            while (cur != nullptr && ans == cur->right)
             {
-                *this = owner->rbegin();
-                return *this;
+                ans = cur;
+                cur = cur->parent;
             }
-            
-            const AVLnode<T>* tmp = node->prevNode();
-
-            if (tmp != nullptr) 
-                node = tmp;
-            
-            return *this;
+            ans = cur;
         }
-        iterator operator-- (int) 
-        {
-            iterator cur = *this;
-            --* this;
-            return cur;
-        }
+        return ans;
+    }
 
-        bool operator== (const iterator& other) const { return (node == other.node && owner == other.owner); }
-        bool operator!= (const iterator& other) const { return (node != other.node || owner != other.owner); }
-    };
-
-    set() = default;
-
-    template<class InputIter>
-    set(InputIter st, InputIter en) 
+    AVLnode* prevNode()
     {
-        while (st != en) 
+        AVLnode* ans = this;
+
+        if (left != nullptr)
         {
-            try 
+            ans = left;
+            while (ans->right != nullptr)
+                ans = ans->right;
+        }
+        else
+        {
+            AVLnode* cur = ans->parent;
+
+            while (cur != nullptr && ans == cur->left)
             {
-                insert(*st);
+                ans = cur;
+                cur = cur->parent;
             }
-            catch (...) 
-            {
-                if (root != nullptr) 
-                    delete root;
-            }
-            ++st;
+            ans = cur;
         }
-    }
-
-    set(const std::initializer_list<T>& inp) { *this = set(inp.begin(), inp.end()); }
-    set(const set& other) { *this = set(other.begin(), other.end()); }
-    ~set() { delete root; }
-
-    void swap(set& rhs) { std::swap(this->root, rhs.root); }
-
-    set& operator= (const set& other) 
-    {
-        set tmp(other); //possible problem
-
-        std::swap(tmp.root, root);
-        sz = tmp.sz;
-        
-        return *this;
-    }
-
-    iterator begin() const { return iterator(body.begin(root), this); }
-    iterator end() const { return iterator(nullptr, this); }
-    iterator rbegin() const { return iterator(body.rbegin(root), this); }
-
-    iterator find(const T& key) const { return iterator(body.find(root, key), this); }
-
-    iterator lower_bound(const T& key) const { return iterator(body.lower_bound(root, key), this); }
-
-    void insert(const T& key) 
-    {
-        if (find(key) != end()) 
-            return;
-
-        root = body.insertNode(root, key);
-        if (root != nullptr)
-            root->parent = nullptr;
-        ++sz;
-    }
-
-    void erase(const T& key) 
-    {
-        root = body.removeNode(root, key);
-        if (root != nullptr)
-            root->parent = nullptr;
-        --sz;
-    }
-
-    bool empty() const { return (sz == 0); }
-    size_t size() const { return sz; }
-
-    // Union.
-    template <typename T>
-    std::shared_ptr<set<T>> operator+(const set<T>& s)
-    {
-        auto d = std::make_shared<set<T>>();
-
-        for (auto i = this->begin(); i != this->end(); ++i)
-            d->insert(*i);
-
-        if (this == &s)
-            return std::make_shared<set<T>>(*this);
-
-        for (auto i = s.begin(); i != s.end(); ++i)
-            d->insert(*i);
-
-        return d;
-    }
-
-    // Intersection.
-    template <typename T>
-    std::shared_ptr<set<T>> operator&(const set<T>& s)
-    {
-        auto d = std::make_shared<set<T>>();
-
-        if (this == &s)
-        {
-            for (auto it = this->begin(); it != this->end(); ++it)
-                d->insert(*it);
-
-            return d;
-        }
-
-        auto s1 = s.begin(), s2 = this->begin();
-
-        while (s1 != s.end() && s2 != this->end())
-        {
-            if (*s1 < *s2)
-                ++s1;
-            else if (*s2 < *s1)
-                ++s2;
-            else //if (*s1 == *s2)
-            {
-                d->insert(*s1);
-                ++s1;
-                ++s2;
-            }
-        }
-
-        return d;
-    }
-
-    // Differnece.
-    template <typename T>
-    std::shared_ptr<set<T>> operator-(const set<T>& s)
-    {
-        auto d = std::make_shared<set<T>>();
-
-        if (this != &s)
-        {
-            auto s1 = this->begin(), s2 = s.begin();
-
-            while (s1 != this->end() && s2 != s.end())
-            {
-                if (*s1 < *s2)
-                {
-                    d->insert(*s1);
-                    ++s1;
-                }
-                else if (*s2 < *s1)
-                {
-                    d->insert(*s2);
-                    ++s2;
-                }
-                else
-                {
-                    ++s1;
-                    ++s2;
-                }
-            }
-
-            for (auto it = s2; it != s.end(); ++it)
-                d->insert(*it);
-        }
-        
-        return d;
+        return ans;
     }
 };
+
+
+template <class T>
+class AVLTree
+{
+private:
+    AVLnode<T>* rotateLeft(AVLnode<T>* a)
+    {
+        if (a == nullptr) 
+            return a;
+        AVLnode<T>* b = a->right;
+        a->right = b->left;
+        b->left = a;
+
+        update(a);
+        update(b);
+
+        return b;
+    }
+
+    AVLnode<T>* rotateRight(AVLnode<T>* a)
+    {
+        if (a == nullptr)
+            return a;
+
+        AVLnode<T>* b = a->left;
+        a->left = b->right;
+        b->right = a;
+
+        update(a);
+        update(b);
+
+        return b;
+    }
+
+    AVLnode<T>* rotateLeftThenRight(AVLnode<T>* cur)
+    {
+        cur->left = rotateLeft(cur->left);
+        return rotateRight(cur);
+    }
+
+    AVLnode<T>* rotateRightThenLeft(AVLnode<T>* cur)
+    {
+        cur->right = rotateRight(cur->right);
+        return rotateLeft(cur);
+    }
+
+    int height(AVLnode<T>* cur)
+    {
+        if (cur == nullptr)
+            return 0;
+        return cur->height;
+    }
+
+    void update(AVLnode<T>* cur)
+    {
+        if (cur == nullptr)
+            return;
+
+        if (cur->left)
+            cur->left->parent = cur;
+        
+        if (cur->right)
+            cur->right->parent = cur;
+        
+        cur->height = std::max(height(cur->left), height(cur->right)) + 1;
+        cur->balance = height(cur->right) - height(cur->left);
+    }
+
+    AVLnode<T>* rebalance(AVLnode<T>* cur)
+    {
+        if (cur == nullptr)
+            return nullptr;
+
+        update(cur);
+
+        if (cur->balance == -2)
+        {
+            if (cur->left->balance < 0)
+                cur = rotateRight(cur);
+            else
+                cur = rotateLeftThenRight(cur);
+        }
+        else if (cur->balance == 2)
+        {
+            if (cur->right->balance > 0)
+                cur = rotateLeft(cur);
+            else
+                cur = rotateRightThenLeft(cur);
+        }
+        return cur;
+    }
+
+    AVLnode<T>* removeMin(AVLnode<T>* cur)
+    {
+        if (cur->left == nullptr)
+            return cur->right;
+
+        cur->left = removeMin(cur->left);
+        cur = rebalance(cur);
+
+        return cur;
+    }
+
+public:
+    AVLTree(void) { }
+    ~AVLTree(void) { }
+
+    AVLnode<T>* insertNode(AVLnode<T>* cur, const T& key)
+    {
+        if (cur == nullptr)
+        {
+            AVLnode<T>* tmp = nullptr;
+
+            tmp = new AVLnode<T>(key);
+            return tmp;
+        }
+        
+        if (comp(cur->key, key) == 1)
+            cur->left = insertNode(cur->left, key);
+        else
+            cur->right = insertNode(cur->right, key);
+
+        cur = rebalance(cur);
+
+        return cur;
+    }
+
+    AVLnode<T>* removeNode(AVLnode<T>* cur, const T& key)
+    {
+        if (cur == nullptr)
+            return cur;
+
+        if (comp(cur->key, key) > 0) 
+            cur->left = removeNode(cur->left, key);
+        else if (comp(cur->key, key) < 0) 
+            cur->right = removeNode(cur->right, key);
+        else 
+        {
+            AVLnode<T>* lchild = cur->left;
+            AVLnode<T>* rchild = cur->right;
+
+            cur->left = nullptr;
+            cur->right = nullptr;
+            delete cur;
+
+            if (rchild == nullptr) 
+                return lchild;
+            
+            cur = rchild;
+            while (cur->left != nullptr) 
+                cur = cur->left;
+            
+            cur->right = removeMin(rchild);
+            cur->left = lchild;
+        }
+        cur = rebalance(cur);
+        
+        return cur;
+    }
+
+    AVLnode<T>* smallest(AVLnode<T>* cur) const
+    {
+        while (cur->left != nullptr) 
+            cur = cur->left;
+
+        return cur;
+    }
+
+    AVLnode<T>* begin(AVLnode<T>* root) const 
+    {
+        if (root == nullptr) 
+            return nullptr;
+    
+        AVLnode<T>* tmp = smallest(root);
+        
+        return tmp;
+    }
+
+    AVLnode<T>* lower_bound(AVLnode<T>* cur, const T& key) const
+    {
+        if (cur == nullptr) 
+            return nullptr;
+
+        if (comp(cur->key, key) == 1) 
+        {
+            AVLnode<T>* tmp = lower_bound(cur->left, key);
+            if (tmp == nullptr) 
+                return cur;
+            else 
+                return tmp;
+        }
+        else if (comp(cur->key, key) == -1) 
+            return lower_bound(cur->right, key);
+
+        return cur;
+    }
+
+    AVLnode<T>* find(AVLnode<T>* cur, const T& key) const
+    {
+        AVLnode<T>* tmp = lower_bound(cur, key);
+
+        if (tmp == nullptr || comp(tmp->key, key) != 0) 
+            return nullptr;
+        
+        return tmp;
+    }
+};
+
+#endif
